b5874964f4e3d7b9b7c8dbc01c68a3d8cd7e1181
Guillermo Achondo <guillermo.achondo@uc.cl>
2023-05-03 11:58:49 -0400

    initial pwa setup

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+window.addEventListener('load', async e => {
+    await fetchTrending();
+
+    if ('serviceWorker' in navigator) {
+        try {
+            navigator.serviceWorker.register('serviceWorker.js');
+            console.log('SW registered');
+
+        } catch (error) {
+            console.log('SW failed');
+
+        }
+    }
+});
